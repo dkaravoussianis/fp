8fc5304ba4cb3fd5364e982995648bf3af286f66
Terry Hearst <thearst3rd@gmail.com>
2019-10-07 22:16:34 -0400

    Added passport authentication+pwd hashing and salting, and login and signup endpoints

--- server.js
+++ server.js
@@ -1,22 +1,106 @@
-// server.js
-// where your node app starts
+/*
+ * CS 4241 Final Project - Server code
+ * by Terry Hearst, Demi Karavoussianis, Kyle Reese, and Tom White
+ */
 
-// init project
-const express = require('express');
-const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// ####################
+// ## INITIALIZE APP ##
+// ####################
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+const express  = require("express")
+const app      = express()
+const session  = require("express-session")
+const passport = require("passport")
+const Local    = require("passport-local").Strategy
+const pass     = require("pwd")
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+// Serve our compiled react program
+app.use(express.static("dist"))
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// Redirect to the default webpage
+app.get("/", function(request, response)
+{
+	response.sendFile(__dirname + "/dist/index.html")
+})
+
+
+// ####################
+// ## AUTHENTICATION ##
+// ####################
+
+/* TEMP STUFF */
+let users = []
+const findUser = function(username)
+{
+	return users.find({"username": username})
+}
+/* END TEMP STUFF */
+
+passport.use(new Local(function(username, password, done)
+{
+	const user = findUser(username) // TODO use actual function
+	
+	if (user === undefined)
+	{
+		return done(null, false)
+	}
+	
+	pass.hash(password, user.salt).then(function(result)
+	{
+		if (user.hash === result.hash)
+		{
+			done(null, user)
+		}
+		else
+		{
+			done(null, false)
+		}
+	})
+}))
+
+app.use(session({"secret": "top 5 bruh moments", "resave": false, "saveUninitialized": false}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.post(
+	"/login",
+	passport.authenticate("local"),
+	function(req, res)
+	{
+		console.log(res)
+		console.log("user:", req.user)
+	}
+)
+
+app.post(
+	"/signup",
+	function(req, res)
+	{
+		console.log("new user:", req.body.username)
+		
+		// Make sure user does not already exist
+		if (findUser(req.body.username) !== undefined)
+		{
+			res.status(403) // Forbidden
+			res.send()
+			return
+		}
+		
+		pass.hash(req.body.password).then(function(result)
+		{
+			users.push({"username": req.body.username, "hash": result.hash, "salt": result.salt})
+			res.json({"status": "success"})
+		}
+	}
+)
+
+
+// ############
+// ## LISTEN ##
+// ############
+
+const listener = app.listen(process.env.PORT || 3000, function()
+{
+	console.log("Your app is listening on port " + listener.address().port)
+})
